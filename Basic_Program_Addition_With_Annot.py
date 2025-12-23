8a2fc7f7f4f3e09d7e1fc96a7dd234971939a387
Robin Kurt Aquino <robinkurtpersonal@gmail.com>
2025-12-23 17:39:45 +0300

    Add basic program for integer addition with output

---
+++ Basic_Program_Addition_With_Annot.py
@@ -0,0 +1,25 @@
+#program with annotations
+#name & description
+print("Name: Basic_Program_Addition")
+print("Description: adds 2 integers (inputs) and displays the sum.")
+print("")
+
+#basic_program 
+
+#inputs as ints
+integer_one = int(input("Enter the first integer: "))
+integer_two = int(input("Enter the second integer: "))
+
+#the formula adding the 2 integers together
+sumofints = integer_one + integer_two
+
+#the output of sum utilizing an f string
+print(f"The total sum of integers is {sumofints}.")
+
+
+#end of basic_program
+
+
+#signature & trademark
+print("")
+print("Program Finished. Copyright RKCA 2025")
