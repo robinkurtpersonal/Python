b3523a947f326e4c4fddd4a7b1dd594b5d3a76dd
Robin Kurt Aquino <robinkurtpersonal@gmail.com>
2025-12-23 18:42:56 +0300

    Create Basic_Program_Hello_World.py

---
+++ Basic_Program_Hello_World.py
@@ -0,0 +1,23 @@
+#name & description
+print("Name: Basic_Program_Hello_World")
+print("Description: Prints 'Hello World!' in 4 different ways.")
+print("")
+
+#basic_program 
+print("Hello","World!")
+
+print("Hello"+" World!")
+
+print("Hello World!")
+
+w = "world!"
+
+print(f"Hello {w}") 
+
+
+#end of basic_program
+
+
+#signature & trademark
+print("")
+print("Program Finished. Copyright RKCA 2025")
