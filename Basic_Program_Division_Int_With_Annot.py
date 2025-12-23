bbead4fd3afc77de927b02f77fdca9e5ee960d85
Robin Kurt Aquino <robinkurtpersonal@gmail.com>
2025-12-23 20:11:35 +0300

    Add basic integer division program with annotations
    This program takes two integers as input, divides them, and displays the rounded down result.

---
+++ Basic_Program_Division_Int_With_Annot.py
@@ -0,0 +1,23 @@
+#program with annotations
+#name & description
+print("Name: Basic_Program_Division_Int")
+print("Description: Divides 2 ints and displays rounded down int result")
+print("")
+
+#basic_program 
+#inputs for 2 ints
+integer_one = int(input("Enter the first integer: "))
+integer_two = int(input("Enter the second integer: "))
+
+#process (formula for quotient // round down to int)
+quotient = integer_one // integer_two
+
+#printing of quotient (utilizing f - string)
+print(f"The quotient of integers is {quotient}")
+
+#end of basic_program
+
+
+#signature & trademark
+print("")
+print("Program Finished. Copyright RKCA 2025")
