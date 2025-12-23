4da60a03d72b827660f3184dc8e58fe92b7959cb
Robin Kurt Aquino <robinkurtpersonal@gmail.com>
2025-12-23 17:38:22 +0300

    Create Basic_Program_Addition.py

---
+++ Basic_Program_Addition.py
@@ -0,0 +1,20 @@
+#name & description
+print("Name: Basic_Program_Addition")
+print("Description: adds 2 integers (inputs) and displays the sum.")
+print("")
+
+#basic_program 
+integer_one = int(input("Enter the first integer: "))
+integer_two = int(input("Enter the second integer: "))
+
+sumofints = integer_one + integer_two
+
+print(f"The total sum of integers is {sumofints}.")
+
+
+#end of basic_program
+
+
+#signature & trademark
+print("")
+print("Program Finished. Copyright RKCA 2025")
