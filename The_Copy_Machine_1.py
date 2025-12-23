1a05ae28b01235151a579efd7fe642d5315c1ae1
Robin Kurt Aquino <robinkurtpersonal@gmail.com>
2025-12-24 02:48:18 +0300

    Easy copy paste for various Python functions/references

---
+++ The_Copy_Machine_1.py
@@ -0,0 +1,22 @@
+#print function
+print("Piece of Text"")
+print("Piece of Text" + " Another Piece of Text")
+VarPOT = "Text"
+print(f"Piece of {VarPOT}")
+
+
+#INPUTS
+variable_input = input("Enter Input: ")
+variable_input_int = int(input("Enter Integer Input: "))
+variable_input_float = float(input("Enter Float Input"))
+
+#Types (Variables)
+variable_integer = 1
+variable_float = 1.0
+variable_string = "Piece of Text"
+variable_bool_true = True
+variable_bool_false = False
+
+#len function
+variable_len = "Text"
+print(len(variable_len))
