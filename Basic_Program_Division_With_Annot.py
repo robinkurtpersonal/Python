a32ab3b3eb34a71d1e193db74062b68a71b3899c
Robin Kurt Aquino <robinkurtpersonal@gmail.com>
2025-12-23 18:09:03 +0300

    Add basic program for dividing two integers

---
+++ Basic_Program_Division_With_Annot.py
@@ -0,0 +1,24 @@
+#program with annotations
+#name & description
+print("Name: Basic_Program_Division ")
+print("Description: Divide 2 integers (inputs) and displays the quotient.")
+print("")
+
+#basic_program 
+#inputs (in integer format)
+integer_one = int(input("Enter the first integer: "))
+integer_two = int(input("Enter the second integer: "))
+
+#process (formula for quotient)
+quotient = integer_one / integer_two
+
+#printing of quotient (using f-string)
+print(f"The quotient of integers is {quotient}")
+
+
+#end of basic_program
+
+
+#signature & trademark
+print("")
+print("Program Finished. Copyright RKCA 2025")
