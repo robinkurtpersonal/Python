a7f6a10b944eb66bc818dffe128f68184c841a10
Robin Kurt Aquino <robinkurtpersonal@gmail.com>
2025-12-23 18:43:22 +0300

    Create Basic Hello World program with annotations
    This program prints 'Hello World!' in four different ways, demonstrating various print techniques in Python.

---
+++ Basic_Program_Hello_World_With_Annot.py
@@ -0,0 +1,29 @@
+#program with annotations
+#name & description
+print("Name: Basic_Program_Hello_World ")
+print("Description: Prints 'Hello World!' in 4 different ways.")
+print("")
+
+#basic_program 
+
+#use of commas to seperate arguments
+print("Hello","World!")
+
+#use of + to concatenate (needs str conversion for non-str)
+print("Hello"+" World!")
+
+#standard print
+print("Hello World!")
+
+#demonstration of an f-string
+w = "world!"
+
+print(f"Hello {w}") 
+
+
+#end of basic_program
+
+
+#signature & trademark
+print("")
+print("Program Finished. Copyright RKCA 2025")
