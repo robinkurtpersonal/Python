a39abcb2e535530fb51e1e263732140e4c32c895
Robin Kurt Aquino <robinkurtpersonal@gmail.com>
2025-12-23 17:48:02 +0300

    Add basic subtraction program for two integers

---
+++ Basic_Program_Subtraction.py
@@ -0,0 +1,20 @@
+#name & description
+print("Name: Basic_Program_Subtraction ")
+print("Description: Minus 2 integers (inputs) and displays the difference.")
+print("")
+
+#basic_program 
+integer_one = int(input("Enter the first integer: "))
+integer_two = int(input("Enter the second integer: "))
+
+difference = integer_one - integer_two
+
+print(f"The difference of integers is {difference}")
+
+
+#end of basic_program
+
+
+#signature & trademark
+print("")
+print("Program Finished. Copyright RKCA 2025")
