c01f28dfc0cb07ad4194759070eb6ecf2a1e0949
Robin Kurt Aquino <robinkurtpersonal@gmail.com>
2025-12-23 19:53:06 +0300

    Create Basic_Program_Power_With_Annot.py

---
+++ Basic_Program_Power_With_Annot.py
@@ -0,0 +1,23 @@
+#program with annotations
+#name & description
+print("Name: Basic_Program_Power ")
+print("Description:Sets one integer as the base and the other as the exponent.")
+print("")
+
+#basic_program 
+
+#inputs (in integer type)
+integer_one = int(input("Enter the first integer (base): "))
+integer_two = int(input("Enter the second integer (Exponent): "))
+
+#formula = base ** exponent
+power = integer_one ** integer_two
+
+#printing of power (using f-string)
+print(f"The power of the 2 integers (Base, exponent) is {power}.")
+#end of basic_program
+
+
+#signature & trademark
+print("")
+print("Program Finished. Copyright RKCA 2025")
