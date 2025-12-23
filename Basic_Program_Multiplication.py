cb9350a529a27aad09255af1a261f2e85698f6fa
Robin Kurt Aquino <robinkurtpersonal@gmail.com>
2025-12-23 17:58:43 +0300

    Create Basic_Program_Multiplication.py

---
+++ Basic_Program_Multiplication.py
@@ -0,0 +1,18 @@
+print("Name: Basic_Program_Multiplication ")
+print("Description: Multiplies 2 integers (inputs) and displays the product.")
+print("")
+
+#basic_program 
+integer_one = int(input("Enter the first integer: "))
+integer_two = int(input("Enter the second integer: "))
+
+product = integer_one * integer_two
+
+print(f"The product of integers is {product}")
+
+#end of basic_program
+
+
+#signature & trademark
+print("")
+print("Program Finished. Copyright RKCA 2025")
