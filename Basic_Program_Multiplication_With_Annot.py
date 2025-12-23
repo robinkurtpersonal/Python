e74d000303d667e4abb2545ca901cada4031d004
Robin Kurt Aquino <robinkurtpersonal@gmail.com>
2025-12-23 17:59:18 +0300

    Create Basic_Program_Multiplication_With_Annot.py

---
+++ Basic_Program_Multiplication_With_Annot.py
@@ -0,0 +1,23 @@
+#program with annotations
+print("Name: Basic_Program_Multiplication ")
+print("Description: Multiplies 2 integers (inputs) and displays the product.")
+print("")
+
+#basic_program 
+
+#2 inputs (in integer type) 
+integer_one = int(input("Enter the first integer: "))
+integer_two = int(input("Enter the second integer: "))
+
+#formula (multiply them together)
+product = integer_one * integer_two
+
+#output (utilizes f-string)
+print(f"The product of integers is {product}")
+
+#end of basic_program
+
+
+#signature & trademark
+print("")
+print("Program Finished. Copyright RKCA 2025")
