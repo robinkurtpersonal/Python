285c027f403fcafc23c67fb2376644442fcfc639
Robin Kurt Aquino <robinkurtpersonal@gmail.com>
2025-12-23 18:08:08 +0300

    Create Basic_Program_Division.py

---
+++ Basic_Program_Division.py
@@ -0,0 +1,20 @@
+#name & description
+print("Name: Basic_Program_Division ")
+print("Description: Divide 2 integers (inputs) and displays the quotient.")
+print("")
+
+#basic_program 
+integer_one = int(input("Enter the first integer: "))
+integer_two = int(input("Enter the second integer: "))
+
+quotient = integer_one / integer_two
+
+print(f"The quotient of integers is {quotient}")
+
+
+#end of basic_program
+
+
+#signature & trademark
+print("")
+print("Program Finished. Copyright RKCA 2025")
