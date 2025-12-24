9cee50ddbd3182cf4feb18bade1413b84b8b347f
Robin Kurt Aquino <robinkurtpersonal@gmail.com>
2025-12-24 12:45:52 +0300

    Create Basic_Program_Len (Len Function).py

---
+++ String Functions/Basic_Program_Len (Len Function).py
@@ -0,0 +1,20 @@
+#name & description
+print("Name: Basic_Program_Len ")
+print("Description: Reads a string and formulates / displays the value of the string length.")
+print("")
+
+#basic_program 
+string = input("Enter your string: ")
+
+
+#---Use of len() Function
+value_len = len(string)
+
+
+print(f"The length of the string is {value_len}")
+#end of basic_program
+
+
+#signature & trademark
+print("")
+print("Program Finished. Copyright RKCA 2025")
