afa052ee72b930cd721b29424154a037724aaf41
Robin Kurt Aquino <robinkurtpersonal@gmail.com>
2025-12-24 20:32:35 +0300

    Create I1101_L3_Selection.py

---
+++ I1101_L3_Selection.py
@@ -0,0 +1,26 @@
+#name & description
+print("Name: I1101_L3_Selection ")
+print("Description: Waiter asks customers for age | above 18 - give menu / take order ")
+print("")
+
+#----------------------------basic_program-------------------------------- 
+Age = int(input("How old are you? "))
+if Age > 18:
+    print("Show Menu")
+    print("Take Order")
+print("Thank you")
+
+#----------------------------end of basic_program-------------------------
+
+
+#signature & trademark
+print("")
+print("Program Finished. Copyright RKCA 2025")
+
+
+#Age -> input from user
+#Condition 1: if age greater than 18
+#C1-Ins SM 
+#C1-Ins TO
+
+#Always print THNK U
