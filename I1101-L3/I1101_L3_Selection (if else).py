f4f8289ded4d3b5acd4f2d5d41e9619ffd3eb134
Robin Kurt Aquino <robinkurtpersonal@gmail.com>
2025-12-24 21:25:37 +0300

    Create I1101_L3_Selection (if else).py

---
+++ I1101-L3/I1101_L3_Selection (if else).py
@@ -0,0 +1,20 @@
+print("Name: I1101_L3_Selection (if else) ")
+print("Description: ")
+print("")
+
+#----------------------------basic_program-------------------------------- 
+Age = int(input("How old are you? "))
+if Age > 18:
+    print("Show Menu")
+else:
+    print("Show Kid's Menu")
+    
+print("Take Order")
+print("Thank you")
+
+#----------------------------end of basic_program-------------------------
+
+
+#signature & trademark
+print("")
+print("Program Finished. Copyright RKCA 2025")
