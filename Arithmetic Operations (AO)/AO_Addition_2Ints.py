5fd259311c5bca7c8cc27e2df4f15c74e8305ce3
Robin Kurt Aquino <robinkurtpersonal@gmail.com>
2025-12-28 18:09:16 +0300

    Create AO_Addition_2Ints.py

---
+++ Arithmetic Operations (AO)/AO_Addition_2Ints.py
@@ -0,0 +1,9 @@
+print("Name: AO_Addition_2Ints")
+print("Description: Adds 2 Integers together")
+
+Int_1 = int(input("Enter your first integer: "))
+Int_2 = int(input("Enter your second integer: "))
+
+Sum = Int_1 + Int_2
+
+print(f"The total sum of your two integers is {Sum}")
