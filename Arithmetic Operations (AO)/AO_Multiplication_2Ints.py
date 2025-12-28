3e77efd8a0772e43b842ee85f3a4bb041feeefca
Robin Kurt Aquino <robinkurtpersonal@gmail.com>
2025-12-28 18:14:02 +0300

    Create AO_Multiplication_2Ints.py

---
+++ Arithmetic Operations (AO)/AO_Multiplication_2Ints.py
@@ -0,0 +1,9 @@
+print("Name: AO_Multiplication_2Ints")
+print("Description: Multiplies 2 Integers together")
+
+Int_1 = int(input("Enter your first integer: "))
+Int_2 = int(input("Enter your second integer: "))
+
+product = Int_1 * Int_2 
+
+print(f"The product of your two integers is {product}")
