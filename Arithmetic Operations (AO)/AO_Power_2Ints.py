26e3753a414d6ca15b842b62fdefd54caaa022f8
Robin Kurt Aquino <robinkurtpersonal@gmail.com>
2025-12-29 12:56:38 +0300

    Create AO_Power_2Ints.py

---
+++ Arithmetic Operations (AO)/AO_Power_2Ints.py
@@ -0,0 +1,9 @@
+print("Name: AO_Power_2Ints")
+print("Description: Solves the power using a base and a exponent")
+
+Int_1 = int(input("Enter the Base: "))
+Int_2 = int(input("Enter th Exponent: "))
+
+power = Int_1 ** Int_2
+
+print(f"The power of your base and exponent is {power}.")
