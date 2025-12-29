dedd5441e904c0228f69aa4773a615a7615ff916
Robin Kurt Aquino <robinkurtpersonal@gmail.com>
2025-12-29 12:58:42 +0300

    Create AO_Division_2Ints_float.py

---
+++ Arithmetic Operations (AO)/AO_Division_2Ints_float.py
@@ -0,0 +1,9 @@
+print("Name: AO_Division_2Ints_float")
+print("Description: Divides 2 integers together (returns float)")
+
+Int_1 = int(input("Enter your first integer: "))
+Int_2 = int(input("Enter your second integer: "))
+
+quotient = Int_1 / Int_2
+
+print(f"The quotient of your two integers is {quotient}")
