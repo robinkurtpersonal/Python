08c4f3081618ba6eb7b4b319ecdbdf3ae18fb6bb
Robin Kurt Aquino <robinkurtpersonal@gmail.com>
2025-12-29 12:22:01 +0300

    Create Print Function (Basic).py

---
+++ Basic Python Syntax (BSY)/Print Function (Basic).py
@@ -0,0 +1,15 @@
+#demonstration of Print Function (Basic)
+
+
+#standard print
+print("Hello World!")
+
+#seperate argument commas
+print("Hello","World!")
+
+#Concatenation (String Operation)
+print("Hello"+" "+"World!")
+
+#f-string (variable inside a string)
+w = "World!"
+print(f"Hello {w}")
