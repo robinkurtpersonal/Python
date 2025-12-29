38a02497854d037c98b0556ded2d3fe27d35a463
Robin Kurt Aquino <robinkurtpersonal@gmail.com>
2025-12-29 12:33:06 +0300

    Create BSY_Variable Naming Conventions.py

---
+++ Basic Python Syntax (BSY)/BSY_Variable Naming Conventions.py
@@ -0,0 +1,29 @@
+#allowed variable names
+
+
+variable1 = "Hi"
+print(variable1)
+
+variable_und = "Hello"
+print(variable_und)
+
+_ = "underscore _"
+print(_)
+
+variabledig123 = "Robin"
+print(variabledig123)
+
+variable_123 = "Yep"
+print(variable_123)
+
+
+#Not Allowed
+
+#SyntaxError: invalid syntax
+#if = "hi" ---------- Reserved not allowed
+
+#SyntaxError: invalid decimal literal
+#123_digit = "Robin" ---- Number first not allowed
+
+#SyntaxError: invalid syntax
+#$100 or 300$ -------- Symbols not allowed
