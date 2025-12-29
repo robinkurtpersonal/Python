308fcfb2dec51234e689b5c80ac3c15bc50a50f2
Robin Kurt Aquino <robinkurtpersonal@gmail.com>
2025-12-29 12:26:48 +0300

    Create BSY_Common Variable Types.py

---
+++ Basic Python Syntax (BSY)/BSY_Common Variable Types.py
@@ -0,0 +1,26 @@
+#Common Variable Types
+
+
+#string == str
+string = "Robin Kurt"
+print(string)
+
+#integer == int
+integer = 10
+print(integer)
+
+#floatingpoint == float
+floatingpoint = 1.5
+print(floatingpoint)
+
+#boolean == bool
+boolean = True
+print(boolean)
+
+
+#check the types
+#format: <class 'type'>
+print(type(string))
+print(type(integer))
+print(type(floatingpoint))
+print(type(boolean))
