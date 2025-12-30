d05dc6fa372c5cd5f27de7077f59d0f352e3498c
Robin Kurt Aquino <robinkurtpersonal@gmail.com>
2025-12-30 20:12:05 +0300

    Create AO_Relational_Operators.py

---
+++ Arithmetic Operations (AO)/AO_Relational_Operators.py
@@ -0,0 +1,26 @@
+print("AO_Relational_Operators")
+
+
+#greater than >
+if 10 > 5:
+    print("10 > 5")
+
+#less than <
+if 20 < 30:
+    print("20 < 30")
+#equal to ==
+if 10 == 10:
+    print("10 == 10")
+
+#not equal to !=
+if 10 != 0:
+    print("10 != 0")
+    
+#greater than or equal to >=
+if 20 >= 10:
+    print("20 >= 10")
+#less than or equal to <=
+
+if 10 <= 20:
+    print("10 <= 20")
+    
