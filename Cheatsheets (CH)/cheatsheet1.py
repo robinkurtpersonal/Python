07a0809a667de63dcab472b0cbc49eb87054a201
Robin Aquino <robinkurtpersonal@gmail.com>
2026-01-03 22:41:24 +0300

    Create cheatsheet1.py

---
+++ Cheatsheets (CH)/cheatsheet1.py
@@ -0,0 +1,33 @@
+#this is a comment
+
+#Several ways to print
+
+#comma method (seperate arguments)
+print("Hello!","World!")
+
+#concatenation for strings
+print("Hello"+" "+"World")
+
+#variables
+num = 2 # int == integer
+dec = 2.0 # float == decimal (floating point number)
+sentence = "Robin" # str == string
+b = True # bool == boolean (True/False)
+
+#variable naming conventions
+vartextonly = "check"
+_varundstart = "check"
+varundend_ = "check"
+number2 = "check"
+all_in_3 = "check"
+
+# 2var = "no" WILL NOT WORK (number)
+# $var = "no" WILL NOT WORK (symbol)
+#if = "no" WILL NOT WORK (reserved word)
+
+#Variables Case Sensitivity
+Variable = "a"
+variable = "b"
+# These are different
+
+#Numberic Expressions
