e97ad26340fdbc277250e751cdea88d805dce9ce
Robin Aquino <robinkurtpersonal@gmail.com>
2026-01-04 19:52:05 +0300

    Create if in - if True - if False.py

---
+++ Basic Python Syntax (BSY)/if in - if True - if False.py
@@ -0,0 +1,11 @@
+#if in -> just checks if the 2nd thing here has the first thing
+if "U" in "UDST":
+    print(1)
+
+#if True
+if True:
+    print(1)
+    
+#if False - skipped (nothing will be printed)
+if False:
+    print(1)
