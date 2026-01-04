c229817dbf2984332d63a8414177f54f5cacfc8b
Robin Aquino <robinkurtpersonal@gmail.com>
2026-01-04 19:18:37 +0300

    Create print_type.py

---
+++ Basic Python Syntax (BSY)/print_type.py
@@ -0,0 +1,15 @@
+#print(type(x))
+#format <class 'datatype'>
+x = "a"
+print(type(x))
+x = 4
+print(type(x))
+x = 4.0
+print(type(x))
+x = True
+print(type(x))
+
+#input function | enter something first | type
+print(type(input("Enter: ")))
+print(type(int(input("Enter: "))))
+print(type(float(input("Enter: "))))
