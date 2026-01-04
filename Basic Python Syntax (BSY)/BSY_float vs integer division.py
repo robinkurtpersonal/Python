2111f3ea86524c82c16ea186d2a77655fc643616
Robin Aquino <robinkurtpersonal@gmail.com>
2026-01-04 19:26:59 +0300

    Create BSY_float vs integer division.py

---
+++ Basic Python Syntax (BSY)/BSY_float vs integer division.py
@@ -0,0 +1,8 @@
+#float division vs integer division
+
+variable = 5
+
+#operation | float division | syntax: /
+print(variable/2)
+#operation | integer division (rounded-down) | syntax: //
+print(variable//2)
