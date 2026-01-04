a2e5d58d87507b96a8496b79db28026562122745
Robin Aquino <robinkurtpersonal@gmail.com>
2026-01-04 18:39:03 +0300

    Create Python Comparison Operators.py

---
+++ Python Fundamentals (Theory integration)/Python Comparison Operators.py
@@ -0,0 +1,31 @@
+#Python Comparison Operators
+#usually done for conditions
+#equal to ==
+if 1 == 1:
+    print("1 is equal to 1")
+    
+#not equal to != 
+if 1 != 0:
+    print("1 is not equal to zero")
+
+#greater than >
+if 60 > 50:
+    print("50 greater than 60")
+
+#less than <
+if 50 < 60:
+    print("50 is less than 60")
+
+#greater than or equal to >=
+if 50 >= 50:
+    print("yes")
+
+if 50 >= 40:
+    print("yes")
+#less than or equal to <=
+if 50 <= 50:
+    print("yes")
+
+if 40 <= 50:
+    print("yes")
+
