ee8b88a6d86a20b9de491994e3e66526312da663
Robin Aquino <robinkurtpersonal@gmail.com>
2026-01-04 19:24:59 +0300

    Create float_int_relationship.py

---
+++ Basic Python Syntax (BSY)/float_int_relationship.py
@@ -0,0 +1,12 @@
+variable = 4
+
+
+#all of the involved == integers
+
+y = 5 * variable + 2
+print(y) #output: 22
+
+#if we had at least one float == overrides fr as float
+y = 5 * variable + 2.0
+print(y)
+
