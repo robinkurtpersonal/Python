ce7b5e2bb71715aaeeaf1f895bb3b631f8de806e
Robin Aquino <robinkurtpersonal@gmail.com>
2026-01-07 11:27:03 +0300

    Create simplecountedloop.py

---
+++ Counted Loops (CL)/simplecountedloop.py
@@ -0,0 +1,15 @@
+#counted loop basic example
+
+#initialization
+number_of_iteration = 0
+
+#condition | counted loop | < 10
+while number_of_iteration < 10:
+    
+    #output
+    print("Robin")
+    #incrementation
+    number_of_iteration += 1
+    
+    
+#program runs from 0-9 exits as 10
