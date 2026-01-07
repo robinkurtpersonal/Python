69165f12fe8af3b5fe2b19798f1ce9bf96b01a48
Robin Aquino <robinkurtpersonal@gmail.com>
2026-01-07 11:52:47 +0300

    Create simple_counted_loops.py

---
+++ Counted Loops (CL)/simple_counted_loops.py
@@ -0,0 +1,21 @@
+print("The Alphabet with loops")
+variable = "abcdefghijklmnopqrstuvwxyz"
+
+counter = 0
+while counter < 26:
+    print(variable[counter])
+    counter = counter + 1
+    
+print("The number with loops")
+counter = 1
+while counter <= 10:
+    print(counter)
+    counter+=1
+    
+print("R to the O to B I N")
+name = "ROBIN"
+counter = 0
+while counter < 5:
+    print(name[counter])
+    counter += 1
+    
